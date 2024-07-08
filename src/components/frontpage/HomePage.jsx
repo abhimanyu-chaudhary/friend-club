dca4a26d301de4167cae9f4d54a55157bf44d039
Abhimanyu Chaudhary <146760746+abhimanyu-chaudhary@users.noreply.github.com>
2024-07-08 23:21:56 +0545

    New Section Added

--- src/components/frontpage/HomePage.jsx
+++ src/components/frontpage/HomePage.jsx
@@ -1,9 +1,11 @@
 import Login from "../login/Login";
+import TopSection from "./TopSection";
 
 function HomePage(){
     return(
         <div className="max-w-screen-xl mx-auto text-xl font-bold">
             <Login />
+            <TopSection />
         </div>
     )
 }
---
+++ src/components/frontpage/TopSection.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function TopSection() {
+  return (
+    <div className='flex mt-10 justify-between'>
+      <div className='h-[70vh] w-[30vw] border-2 border-slate-950 rounded-md'>
+
+      </div>
+      <div className=''>
+        <h1 className='font-bold text-7xl select-none'>FRIENDS FOREVER</h1>
+      </div>
+    </div>
+  )
+}
+
+export default TopSection;
